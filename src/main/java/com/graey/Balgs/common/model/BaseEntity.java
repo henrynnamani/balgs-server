5bd8c6b610964b555f5d558d02bebf3b20b6ddee
henrynnamani <hoyx0101@gmail.com>
2026-05-11 12:46:54 +0100

    fix: baseEntity model

--- src/main/java/com/graey/Balgs/common/model/BaseEntity.java
+++ src/main/java/com/graey/Balgs/common/model/BaseEntity.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(updatable = false, nullable = false)
+    @Column(updatable = false, nullable = false, columnDefinition = "uuid")
     private UUID id;
 
     @CreatedDate
