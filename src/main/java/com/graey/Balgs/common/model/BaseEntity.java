b61013b7fa7d8d7dc73cc45f8315ce2dde734a84
henrynnamani <hoyx0101@gmail.com>
2026-05-11 12:47:21 +0100

    fix: baseEntity model

--- src/main/java/com/graey/Balgs/common/model/BaseEntity.java
+++ src/main/java/com/graey/Balgs/common/model/BaseEntity.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
